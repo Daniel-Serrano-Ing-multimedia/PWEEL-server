03f736267ee9edc7364ad0493928f6c1f0c43fd7
Daniel_Serrano <ingm.daniel.serrano@gmail.com>
2020-12-08 17:50:55 -0500

    activar / desactivar usuario

--- controllers/users.js
+++ controllers/users.js
@@ -177,7 +177,22 @@ const getAvatar = ( req, res ) =>{
 }
 
 const activateUser = ( req, res ) =>{
-    console.log ( "activateUser..." );
+    const { id } = req.params;
+    const { active } = req.body;
+    User.findByIdAndUpdate( id, { active }, ( err, userStored ) =>{
+        if ( err ) {
+            res.status(500).send({ code: 500, message: 'Error del servidor.' });
+        } else if ( !userStored ) {
+            res.status(404).send({ code: 404, message: 'Usuario no encontrado.' });
+        }else{
+            if ( active ) {
+                res.status(200).send({ code: 200, message: 'Usuario activado.' });
+            } else {
+                res.status(200).send({ code: 200, message: 'Usuario desactivado.' });
+            }
+        }
+
+    } )
 } 
 
 module.exports = {
--- routes/users.js
+++ routes/users.js
@@ -10,9 +10,9 @@ const api =  express.Router();
 api.post( "/sign-up", UserController.signUp );
 api.post( "/sign-in", UserController.signIn );
 api.put( "/update-user/:id", [ md_auth.ensureAuth ], UserController.updateUser );
+api.put( "/activate-user/:id", [ md_auth.ensureAuth ], UserController.activateUser );
 api.delete( "/delete-user/:id", [ md_auth.ensureAuth ], UserController.deleteUser );
 api.put( "/upload-avatar/:id", [ md_auth.ensureAuth, md_upload_avatar ], UserController.uploadAvatar );
-api.put( "/activate-user/:id", [ md_auth.ensureAuth ], UserController.activateUser );
 api.get( "/get-avatar/:avatarName", UserController.getAvatar );
 
 module.exports= api;
