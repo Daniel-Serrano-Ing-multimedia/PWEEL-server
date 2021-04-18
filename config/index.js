85e645c71a207acc21222fb9747d1614c01ded30
Daniel_Serrano <ingm.daniel.serrano@gmail.com>
2021-04-18 12:03:06 -0500

    reestructurando api

--- config/index.js
+++ config/index.js
@@ -2,12 +2,10 @@ require('dotenv').config ({ path : 'variables.env' });
 const cloudinary = require('cloudinary').v2;
 const API_NAME    = "pweel-api";
 const API_VERSION = "v1";
-const IP_SERVER   = "localhost";
-const PORT_DB     = 27017;
 //
-const CLUSTER_USER      = "Daniel";
-const CLUSTER_PASSWORD  = "daniel1234";
-const CLUSTER_DATA_BASE = "PWEELDB";
+const CLUSTER_USER      = process.env.CLUSTER_USER;
+const CLUSTER_PASSWORD  = process.env.CLUSTER_PASSWORD;
+const CLUSTER_DATA_BASE = process.env.CLUSTER_DATA_BASE;
 //
 const SECRET_KEY = 'gr7HTYU182d56dSAFd54try568821';
 
@@ -20,8 +18,6 @@ const cloudinaryConfig = {
 module.exports = {
   API_NAME,
   API_VERSION,
-  IP_SERVER,
-  PORT_DB,
   CLUSTER_USER,
   CLUSTER_PASSWORD,
   CLUSTER_DATA_BASE,
--- index.js
+++ index.js
@@ -1,26 +1,37 @@
-const mongoose = require("mongoose");
-const app = require("./app");
+const express = require ('express');
+const conetcarDB = require('./config/db');
+const cors = require('cors');
+//config
+const { API_NAME, API_VERSION } = require("./config");
+// routes
+const usersRoutes = require( './routes/users' );
+const authRoutes = require( './routes/auth' );
+const laborsRoutes = require( './routes/labors' );
+const employRoutes = require( './routes/employ' );
+const employerRoutes = require( './routes/employer' );
+// crear el servidor
+const app = express();
+//conectar a la base de datos
+conetcarDB();
+// habilitar Cors
+console.log ( 'FrontEnd : ', process.env.FRONTEND_URL )
+const opcionesCors = {
+  origin: process.env.FRONTEND_URL
+}
+app.use( cors( opcionesCors ) );
+// puerto de la app
 const port = process.env.PORT || 3977;
-
-const { API_NAME, API_VERSION, IP_SERVER, CLUSTER_USER, CLUSTER_PASSWORD, CLUSTER_DATA_BASE } = require("./config");
-
-//mongoose.connect( `mongodb://localhost:27017/daneilalejandroserrano` );
-//conexion de server a basse de datos
-mongoose.set("useFindAndModify", false);
-mongoose.connect(
-  `mongodb+srv://${ CLUSTER_USER }:${ CLUSTER_PASSWORD }@webpersonalcurso.ra4ql.mongodb.net/${ CLUSTER_DATA_BASE }?retryWrites=true&w=majority`,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err, res) => {
-    if (err) {
-      throw err;
-    } else {
-      console.log("la conexiona a la BD es correcta");
-      app.listen(port, () => {
-        console.log("#############################");
-        console.log("########## API REST #########");
-        console.log("#############################");
-        console.log(`http://${ IP_SERVER }:${ port }/${ API_NAME }/${ API_VERSION }/`);
-      });
-    }
-  }
-);
+// habilitar leer valores de un body
+app.use( express.json() );
+// habilitar carpeta publica
+app.use( express.static( 'uploads' ) );
+// rutas app
+app.use( `/${ API_NAME }/${ API_VERSION }`, usersRoutes );
+app.use( `/${ API_NAME }/${ API_VERSION }`, authRoutes );
+app.use( `/${ API_NAME }/${ API_VERSION }`, laborsRoutes );
+app.use( `/${ API_NAME }/${ API_VERSION }`, employRoutes );
+app.use( `/${ API_NAME }/${ API_VERSION }`, employerRoutes );
+// Arrancar la app
+app.listen(port, '0.0.0.0', () => {
+  console.log( `El servidor esta funcionando en el puerto ${ port }` );
+}  )
